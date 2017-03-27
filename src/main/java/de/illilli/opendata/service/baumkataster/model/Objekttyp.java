572f1661603a609dabe6e2b4d10bf094e68ccb65
Wolfram Eberius <eberius@gmail.com>
2017-03-27 18:19:18 +0200

    Mapping für objekttyp erstellen #2

---
+++ src/main/java/de/illilli/opendata/service/baumkataster/model/Objekttyp.java
@@ -0,0 +1,60 @@
+package de.illilli.opendata.service.baumkataster.model;
+
+public class Objekttyp {
+
+	private Integer id;
+	private String text;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((text == null) ? 0 : text.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Objekttyp other = (Objekttyp) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (text == null) {
+			if (other.text != null)
+				return false;
+		} else if (!text.equals(other.text))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Objekttyp [id=" + id + ", text=" + text + "]";
+	}
+
+}
